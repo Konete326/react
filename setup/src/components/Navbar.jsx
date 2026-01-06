b600708293244d0f3fb65871da1c1b056d1e15c4
konete326 <sameerakramsameerakram01@gmail.com>
2026-01-06 21:09:12 +0500

    add setup min  files

---
+++ setup/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+   <>
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
+
+
+   </>
+  )
+}
+
+export default Navbar
